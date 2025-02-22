b17a38c43677b227a43decf0e4b83612c03cf070
Jeff Mitchell <ghlengineer@ghl.ca>
2025-02-21 16:05:46 -0800

    improvement: static_asset handler integration test completed

--- server/tests/integration/static_asset.rs
+++ server/tests/integration/static_asset.rs
@@ -7,25 +7,33 @@ use pavex::http::StatusCode;
 async fn static_asset_works() {
     let api = TestApi::spawn().await;
 
-    let filenames = ["text.css", "image.ico", "script.js"];
+    let test_cases = [
+        ("text.css", "text/css"),
+        ("image.ico", "image/x-icon"),
 
-    for file in filenames.iter().enumerate() {
-        let response = api.get_static_asset(file.1).await;
+        ("script.js", "text/javascript"),
+    ];
 
-        assert_eq!(response.status(), StatusCode::OK);
+    for (filename, expected_content_type) in test_cases.iter() {
+        let response = api.get_static_asset(filename).await;
+
+        assert_eq!(response.status(), StatusCode::OK, "Unexpected status code for {}", filename);
 
         let response_header = response
             .headers()
             .get("Content-Type")
-            .expect("Expected Content-Type header in response.");
+            .expect(&format!("Expected Content-Type header in response for {}", filename));
 
         let response_header_str = response_header
             .to_str()
-            .expect("Unable to get the response header text");
+            .expect("Unable to convert Content-Type header to string");
 
-        let expected_header_parts: Vec<&str> = file.1.split('.').collect();
-        let expected_header_str = format!("{}/{}", expected_header_parts[0], expected_header_parts[1]);
-
-        assert_eq!(response_header_str, &expected_header_str);
+        assert_eq!(
+            response_header_str, 
+            *expected_content_type,
+            "Unexpected Content-Type for {}: expected {}, got {}",
+            filename, expected_content_type, response_header_str
+        );
     }
 }
+
